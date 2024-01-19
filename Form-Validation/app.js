9ec3a088b6ef3d8660cda5e13b8c91b53be42186
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-19 21:45:50 +0300

    feat: add func  validateEmail

--- Form-Validation/app.js
+++ Form-Validation/app.js
@@ -1,7 +1,7 @@
 const nameError = document.querySelector('#name-error');
 const passwordError = document.querySelector('#password-error');
 const emailError = document.querySelector('#email-error');
-const telError = document.querySelector('#tel-error');
+const phoneError = document.querySelector('#phone-error');
 
 const validateName = () => {
     const name = document.querySelector('#name').value;
@@ -28,16 +28,30 @@ const validatePhone = () => {
         return false;
     }
 
-    if (phone.lenght !== 10) {
-        phoneError.innerHTML = 'Номер должен быть больше 10 цифр';
+    if (phone.lenght !== 11) {
+        phoneError.innerHTML = 'Номер должен быть из 11 цифр';
         return false;
     }
 
-    if (!phone.match(/^[0-9]{10}$/)) {
-        phoneError.innerHTML = 'Неверно';
+    if (!phone.match(/^[0-9]{11}$/)) {
+        phoneError.innerHTML = 'Только цифры';
         return false;
     }
 
     phoneError.innerHTML = '<ion-icon name="checkmark-circle-outline"></ion-icon>';
     return true;
+}
+
+const validateEmail = () => {
+    const email = document.querySelector('#email').value;
+
+    if (email.lenght == 0) {
+        email.innerHTML = 'Требуется почта';
+        return false;
+    }
+
+    if (!phone.match(/^[0-9]{11}$/)) {
+        emailError.innerHTML = 'Неверная почта';
+        return false;
+    }
 }
