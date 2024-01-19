828f46c0a35613524c12ccfeb615d702956c52bf
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-19 20:56:33 +0300

    feat: add var js

---
+++ Form-Validation/app.js
@@ -0,0 +1,4 @@
+const nameError = document.querySelector('name-error');
+const passwordError = document.querySelector('password-error');
+const emailError = document.querySelector('email-error');
+const telError = document.querySelector('tel-error');
