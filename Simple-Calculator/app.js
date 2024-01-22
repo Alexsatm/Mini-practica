7627b5f5150af046f536c00301f732bb5ba3cbcc
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-22 23:46:37 +0300

    feat: js function makeOperation

--- Simple-Calculator/app.js
+++ Simple-Calculator/app.js
@@ -3,3 +3,24 @@ let result = document.querySelector('.result');
 
 let input1 = document.getElementById('number1')
 let input2 = document.getElementById('number2')
+
+function makeOperation(operationCode){
+    let number1 = Number(input1.value);
+    let number2 = Number(input2.value);
+
+    if(operationCode === '+'){
+        result.innerHTML = number1 + number2;
+    }
+    if (operationCode === '-'){
+        result.innerHTML = number1 - number2;
+    }
+    if (operationCode === '*'){
+        result.innerHTML = number1 * number2;
+    }
+    if (operationCode === '/'){
+        result.innerHTML = number1 / number2;
+    }
+    if (operationCode === '%'){
+        result.innerHTML = number1 % number2;
+    }
+}
