27dc7aff67d4e4d4dcb7f4ae5931abea6c6e3778
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-22 23:46:16 +0300

    feat: js var

---
+++ Simple-Calculator/app.js
@@ -0,0 +1,5 @@
+let operationBtns = document.querySelectorAll('.operation-btn');
+let result = document.querySelector('.result');
+
+let input1 = document.getElementById('number1')
+let input2 = document.getElementById('number2')
