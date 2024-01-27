4562b569bf717c431da780900da6ac8bb762ce85
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-27 18:40:58 +0300

    feat: function loadSong

--- Music-Player/app.js
+++ Music-Player/app.js
@@ -10,3 +10,19 @@ const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
 const songs = ['audio-1', 'audio-2', 'audio-2'];
+
+let songIndex = 2;
+
+loadSong(songs[songIndex])
+
+function loadSong (song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `img/${song}.jpg`;
+}
+
+//event listenner
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+})
