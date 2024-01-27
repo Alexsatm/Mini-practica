52d3cfef7039d157d0b29ebc255698c52b2e8ba6
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-27 18:33:48 +0300

    feat: add js variable and arr

---
+++ Music-Player/app.js
@@ -0,0 +1,12 @@
+const musicContainer = document.querySelector('#container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('#progress');
+const progressContainer = document.querySelector('progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+const songs = ['audio-1', 'audio-2', 'audio-2'];
